55b7d815ff72f8882c5eb0fbe0c91717a4fd689a
allen <allen@littlemonkey.co.nz>
2020-09-02 21:05:32 +1200

    python 101

---
+++ dicts.py
@@ -0,0 +1,9 @@
+mydict = {"name": "Max", "age": 28, "email": "max@xyz.com"}
+
+mydict2 = {3: 9, 6: 36, 9: 81}
+
+mytuple= [8, 7]
+
+mydict3 = {mytuple: 15}
+
+print(mydict3)
---
+++ strings.py
@@ -0,0 +1,5 @@
+my_string ="Hello World"
+
+char = my_string[1]
+
+print(char)
